3ca623af8dc3fdf6ae006088f335b70cb70ad2ab
JAmoMES <atipat.pa@ku.th>
2021-01-26 21:18:46 +0700

    add CK_CK.cpp

---
+++ beforetest/CK_CK.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    x ==2558;
+    int p;
+    while(cin >> p,p){
+        x--;
+    }
+    cout << (x%2? 'C':'K');
+    return 0;
+}
