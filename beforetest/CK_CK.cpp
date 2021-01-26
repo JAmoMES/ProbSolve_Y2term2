d17c039912b02bf8011e0ac2cc5837620a752f3c
JAmoMES <atipat.pa@ku.th>
2021-01-27 02:10:25 +0700

    add short.cpp

--- beforetest/CK_CK.cpp
+++ beforetest/CK_CK.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main(){
     int x;
     cin >> x;
-    x ==2558;
+    x -=2558;
     int p;
-    while(cin >> p,p){
-        x--;
-    }
+    while(cin >> p,p) x--;
     cout << (x%2? 'C':'K');
     return 0;
 }
